429ec2c87f40890fdaf9d0e9049d7b5148f0ec78
Sarra Bannour <103739009+sarrrabannour@users.noreply.github.com>
2023-11-19 14:54:08 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+//import TodoForm from './components/TodoForm';
+import Todolist from './components/Todolist';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <Todolist />
+    </div>
+  )
+}
+
+export default App;
